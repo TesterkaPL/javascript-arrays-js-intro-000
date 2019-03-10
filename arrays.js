e5ec8f1d0113f3c1cb526208b11d01f32aa9d58d
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-10 20:33:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray(array, element){
 
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
-  return array = [...array, 'foo']
+  return array
 }
 
 function accessElementInArray(array, index){
