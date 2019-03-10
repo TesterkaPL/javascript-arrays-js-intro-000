85404fc51ab8d916dcb7e27ec47dc8d6de62059f
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-10 20:19:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  var array = [1]
+  //var array = [1]
   return array = ['foo',  ...array]
 }
 
