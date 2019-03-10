f97fe0f227a02f858ee3f94f88d534fbec3e20fc
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-10 20:06:57 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  var array = [1]
+  return array = ['foo',  ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = [1]
+  return array = ['foo',  ...array]
+}
+
+function addElementToEndOfArray(array, element){
+  var array = [1]
+  return array = [...array, 'foo']
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  return array = [...array, 'foo']
+}
+
+function accessElementInArray(array, index){
+  return array[2]
+}
+
+function destructivelyAddElementToBeginningOfArray(array){
+  var array = ['1', '2']
+  array.slice(1)
+  return array
+}
