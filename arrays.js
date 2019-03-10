e341ad0ac0fa4df7a1c2c2d337e291e1793c39b9
ambitious-waterfall-9648 <porebadorota2@gmail.com>
2019-03-10 20:13:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,8 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return array = ['foo',  ...array]
+  array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element){
